4233a0cf9c2a528a488130fd89c8f89390680145
Rhenan Bartels <rhenan.bartels@gmail.com>
2016-10-20 22:56:31 -0200

    Remove float from division

--- hrv/classical.py
+++ hrv/classical.py
@@ -10,7 +10,7 @@ def time_domain(rri):
     rmssd = np.sqrt(np.mean(diff_rri ** 2))
     sdnn = np.std(diff_rri, ddof=1)  # make it calculates N-1
     nn50 = sum(abs(diff_rri) > 50)
-    pnn50 = nn50 / float(len(diff_rri))
+    pnn50 = nn50 / len(diff_rri)
     mrri = np.mean(rri)
     mhr = np.mean(60 / (rri / 1000.0))
 
