e95aafccff6f070d4cfbc47c7e0181076f0ac22e
Rhenan Bartels <rhenan.bartels@gmail.com>
2016-10-22 15:31:32 -0200

    Add test for texts and hrm rri files

--- hrv/utils.py
+++ hrv/utils.py
@@ -1,7 +1,51 @@
 # coding: utf-8
+from numbers import Number
+import re
+
 import numpy as np
 
+
+class EmptyFileError(Exception):
+    def __init__(self, value):
+        self.value = value
+ 
+    def __str__(self):
+        return repr(self.value)
+
 def validate_rri(rri):
+    is_list_of_numbers(rri)
     return np.array(rri)
+
+def open_rri(pathname_or_fileobj):
+    if isinstance(pathname_or_fileobj, str):
+        rri = open_rri_from_path(pathname_or_fileobj)
+    return validate_rri(rri)
+
+def open_rri_from_path(pathname):
+    if pathname.endswith('.txt'):
+        with open(pathname, 'r') as fileobj:
+            rri_lines = fileobj.readlines()
+            if len(rri_lines) != 0:
+                rri = [float(rri.strip()) for rri in rri_lines]
+            else:
+                raise EmptyFileError('File without rri data')
+        return rri
+    elif pathname.endswith('.hrm'):
+        with open(pathname, 'r') as fileobj:
+            rri_lines = fileobj.read()
+            begin_of_rri_header = '[HRData]'
+            rri_info_index = rri_lines.find(begin_of_rri_header)
+            rri_string = re.findall(r'\d+', rri_lines[rri_info_index:-1])
+            if len(rri_string) != 0:
+                rri = [float(rri.strip()) for rri in rri_string]
+            else:
+                raise EmptyFileError('File without rri data')
+        return rri
+    raise IOError('File extension not supported')
+
+def is_list_of_numbers(rri):
+    if not all(map(lambda value: isinstance(value, Number), rri)):
+        raise ValueError('rri must be a list or numpy.ndarray of numbers')
+        response = open_rri(rri_file_name)
 
 
---
+++ tests/test_utils.py
@@ -0,0 +1,53 @@
+# conding: utf-8
+import unittest
+
+import numpy as np
+
+from hrv.utils import validate_rri, open_rri, EmptyFileError
+
+FAKE_RRI = [800, 810, 815, 750]
+
+
+class RRIValidationTestCase(unittest.TestCase):
+    def test_simple_rri_validation(self):
+        response = validate_rri(FAKE_RRI)
+        self.assertIsInstance(response, np.ndarray)
+
+    def test_rri_is_different_than_numbers(self):
+        string_rri = '100, 300, 400, 500'
+        self.assertRaises(ValueError, validate_rri, string_rri)
+
+    def test_rri_is_list_numbers(self):
+        rri_with_text = FAKE_RRI + ['text']
+        self.assertRaises(ValueError, validate_rri, rri_with_text)
+
+
+class RRIFileOpeningTestCase(unittest.TestCase):
+
+    def test_open_rri_text_file(self):
+        rri_file_name = 'tests/test_files/test_file_1.txt'
+        expected = np.array(FAKE_RRI)
+        response = open_rri(rri_file_name)
+        np.testing.assert_equal(response, expected)
+
+    def test_open_rri_with_not_supported_extension(self):
+        rri_file_name = 'tests/tests_files/test_file_1.bin'
+        self.assertRaises(IOError, open_rri, rri_file_name)
+
+    def test_open_rri_text_file_with_mistake(self):
+        rri_file_name = 'tests/test_files/test_file_mistake_1.txt'
+        self.assertRaises(ValueError, open_rri, rri_file_name)
+
+    def test_open_empty_text_file(self):
+        rri_file_name = 'tests/test_files/empty.txt'
+        self.assertRaises(EmptyFileError, open_rri, rri_file_name)
+
+    def test_open_hrm_file(self):
+        rri_file_name = 'tests/test_files/test_file_2.hrm'
+        response = open_rri(rri_file_name)
+        expected = FAKE_RRI
+        np.testing.assert_equal(response, expected)
+
+    def test_open_hrm_file_with_mistake(self):
+        rri_file_name = 'tests/test_files/test_file_mistake_2.hrm'
+        self.assertRaises(EmptyFileError, open_rri, rri_file_name)
