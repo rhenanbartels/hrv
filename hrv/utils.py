ea97cd64baf50de6653314d558a10a6624a8b9db
Rhenan Bartels <rhenan.bartels@gmail.com>
2016-10-24 21:15:08 -0200

    Add validation for path and fileobj

--- hrv/utils.py
+++ hrv/utils.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+import io
 from numbers import Number
 import re
 
@@ -12,6 +13,15 @@ class EmptyFileError(Exception):
     def __str__(self):
         return repr(self.value)
 
+
+class FileNotSupportedError(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
+
+
 def validate_rri(rri):
     is_list_of_numbers(rri)
     return np.array(rri)
@@ -19,6 +29,8 @@ def validate_rri(rri):
 def open_rri(pathname_or_fileobj):
     if isinstance(pathname_or_fileobj, str):
         rri = open_rri_from_path(pathname_or_fileobj)
+    elif isinstance(pathname_or_fileobj, io.TextIOWrapper):
+        rri = open_rri_from_fileobj(pathname_or_fileobj)
     return validate_rri(rri)
 
 def open_rri_from_path(pathname):
@@ -29,31 +41,46 @@ def open_rri_from_path(pathname):
         with open(pathname, 'r') as fileobj:
             rri = open_rri_from_fileobj(fileobj)
     else:
-        raise IOError("File extension not supported")
+        raise FileNotSupportedError("File extension not supported")
     return rri
 
 def open_rri_from_fileobj(fileobj):
     file_content = fileobj.read()
     file_type = identify_rri_file_type(file_content)
     if file_type == 'text':
-        rri = list(map(float,
-                       re.findall(r'\d+', file_content)))
+        rri = open_rri_from_text(file_content)
         if not rri:
             raise EmptyFileError('File without rri data')
     else:
-        rri_info_index = file_content.find('[HRData]')
-        if rri_info_index >= 0:
-            rri = list(map(float,
-                      re.findall(r'\d+', file_content[rri_info_index:-1])))
-            if not rri:
-                raise EmptyFileError('File without rri data')
+        rri = open_rri_from_hrm(file_content)
+        if not rri:
+            raise EmptyFileError('File without rri data')
     return rri
-# TODO: improve file type identification
+
+def open_rri_from_text(file_content):
+    rri = list(map(float,
+                   re.findall(r'\d+', file_content)))
+    return rri
+
+def open_rri_from_hrm(file_content):
+    rri_info_index = file_content.find('[HRData]')
+    rri = None
+    if rri_info_index >= 0:
+        rri = list(map(float,
+                  re.findall(r'\d+', file_content[rri_info_index:-1])))
+    return rri
+
 def identify_rri_file_type(file_content):
     is_hrm_file = file_content.find('[HRData]')
     if is_hrm_file >= 0:
         file_type = 'hrm'
     else:
+        rri_lines = file_content.split('\n')
+        for line in rri_lines:
+            current_line_number = re.findall(r'\d+', line)
+            if current_line_number:
+                if not current_line_number[0] == line.strip():
+                    raise FileNotSupportedError('Text file not supported')
         file_type = 'text'
     return file_type
 
@@ -61,5 +88,3 @@ def is_list_of_numbers(rri):
     if not all(map(lambda value: isinstance(value, Number), rri)):
         raise ValueError('rri must be a list or numpy.ndarray of numbers')
         response = open_rri(rri_file_name)
-
-
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,7 +4,8 @@ import unittest
 import numpy as np
 
 from hrv.utils import (validate_rri, open_rri, EmptyFileError,
-                       identify_rri_file_type)
+                       identify_rri_file_type, FileNotSupportedError,
+                       open_rri_from_text, open_rri_from_hrm)
 
 FAKE_RRI = [800, 810, 815, 750]
 
@@ -33,7 +34,7 @@ class RRIFileOpeningTestCase(unittest.TestCase):
 
     def test_open_rri_with_not_supported_extension(self):
         rri_file_name = 'tests/tests_files/test_file_1.bin'
-        self.assertRaises(IOError, open_rri, rri_file_name)
+        self.assertRaises(FileNotSupportedError, open_rri, rri_file_name)
 
     def test_open_empty_text_file(self):
         rri_file_name = 'tests/test_files/empty.txt'
@@ -43,6 +44,20 @@ class RRIFileOpeningTestCase(unittest.TestCase):
         rri_file_name = 'tests/test_files/test_file_2.hrm'
         response = open_rri(rri_file_name)
         expected = FAKE_RRI
+        np.testing.assert_equal(response, expected)
+
+    def test_text_open_funtion(self):
+        rri_file_name = 'tests/test_files/test_file_1.txt'
+        file_content = open(rri_file_name).read()
+        expected = FAKE_RRI
+        response = open_rri_from_text(file_content)
+        np.testing.assert_equal(response, expected)
+
+    def test_hrm_open_funtion(self):
+        rri_file_name = 'tests/test_files/test_file_2.hrm'
+        file_content = open(rri_file_name).read()
+        expected = FAKE_RRI
+        response = open_rri_from_hrm(file_content)
         np.testing.assert_equal(response, expected)
 
     def test_open_hrm_file_with_mistake(self):
@@ -55,3 +70,34 @@ class RRIFileOpeningTestCase(unittest.TestCase):
         expected = 'text'
         response = identify_rri_file_type(file_obj.read())
         self.assertEqual(response, expected)
+
+    def test_text_file_not_supported(self):
+        rri_file_name = 'tests/test_files/test_file_2.txt'
+        file_obj = open(rri_file_name, 'r')
+        self.assertRaises(FileNotSupportedError, identify_rri_file_type,
+                          file_obj.read())
+
+    def test_text_file_not_supported_with_words(self):
+        rri_file_name = 'tests/test_files/test_file_3.txt'
+        file_obj = open(rri_file_name, 'r')
+        self.assertRaises(FileNotSupportedError, identify_rri_file_type,
+                          file_obj.read())
+
+    def test_open_rri_from_text_fileobj(self):
+        rri_file_name = 'tests/test_files/test_file_1.txt'
+        file_obj = open(rri_file_name, 'r')
+        expected = FAKE_RRI
+        response = open_rri(file_obj)
+        np.testing.assert_equal(response, expected)
+
+    def test_open_rri_from_hrm_fileobj(self):
+        rri_file_name = 'tests/test_files/test_file_2.hrm'
+        file_obj = open(rri_file_name, 'r')
+        expected = FAKE_RRI
+        response = open_rri(file_obj)
+        np.testing.assert_equal(response, expected)
+
+    def test_open_rri_from_notsupported_fileobj(self):
+        rri_file_name = 'tests/test_files/test_file_3.txt'
+        file_obj = open(rri_file_name, 'r')
+        self.assertRaises(FileNotSupportedError, open_rri, file_obj)
