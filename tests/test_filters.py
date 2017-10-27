2d102e049ceb4ac6d9892313e78b82fc91f9e84c
Rhenan Bartels <rhenan.bartels@gmail.com>
2017-10-27 16:31:52 -0200

    Test moving average filter for 5th order

--- tests/test_filters.py
+++ tests/test_filters.py
@@ -13,3 +13,11 @@ class Filter(TestCase):
 
         expected = [810, 833.33, 826.66, 818, 804]
         np.testing.assert_almost_equal(rri_filt, expected, decimal=2)
+
+    def test_moving_average_order_5(self):
+        fake_rri = np.array([810, 830, 860, 790, 804, 801, 800])
+
+        rri_filt = moving_average(fake_rri, order=5)
+
+        expected = [810, 830, 818.79, 817.0, 811.0, 801, 800]
+        np.testing.assert_almost_equal(rri_filt, expected, decimal=2)
