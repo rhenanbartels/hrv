36cf28d9b7b54f57f2e8c1ffcad2c49ab7a62a30
Rhenan Bartels <rhenan.bartels@gmail.com>
2018-06-11 23:23:15 -0300

    Create time array

--- hrv/rri.py
+++ hrv/rri.py
@@ -2,8 +2,9 @@ import numpy as np
 
 
 class RRi:
-    def __init__(self, rri):
+    def __init__(self, rri, time=None):
         self.rri = _validate_rri(rri)
+        self.time = _create_time_array(self.rri)
 
     @property
     def values(self):
@@ -11,10 +12,14 @@ class RRi:
 
 
 def _validate_rri(rri):
-    # Use RRi series median value to check if it is in seconds or miliseconds
     rri = np.array(rri)
 
-    if np.median(rri) < 100:
-        rri *= 1000
+    # Use RRi series median value to check if it is in seconds or miliseconds
+    if np.median(rri) < 10:
+        rri *= 1000.0
 
     return rri
+
+
+def _create_time_array(rri):
+    return np.cumsum(rri) / 1000.0
--- tests/test_rri.py
+++ tests/test_rri.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from hrv.rri import RRi, _validate_rri
+from hrv.rri import RRi, _validate_rri, _create_time_array
 from tests.test_utils import FAKE_RRI
 
 
@@ -26,3 +26,24 @@ def test_rri_values():
 
     assert isinstance(rri, np.ndarray)
     np.testing.assert_array_equal(rri, np.array(FAKE_RRI))
+
+
+def test_create_time_array():
+    rri_time = _create_time_array(FAKE_RRI)
+
+    assert isinstance(rri_time, np.ndarray)
+    np.testing.assert_array_equal(rri_time, np.cumsum(FAKE_RRI) / 1000)
+
+
+def test_rri_time_auto_creation():
+    rri = RRi(FAKE_RRI)
+
+    np.testing.assert_array_equal(rri.time, np.cumsum(FAKE_RRI) / 1000.0)
+
+
+def test_rri_time_passed_as_argument():
+    rri_time = np.cumsum(FAKE_RRI) / 1000.0
+    rri = RRi(FAKE_RRI, rri_time)
+
+    assert isinstance(rri.time, np.ndarray)
+    np.testing.assert_array_equal(rri.time, np.cumsum(FAKE_RRI) / 1000.0)
