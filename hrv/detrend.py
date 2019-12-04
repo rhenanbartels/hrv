c9f11b905e3223245f8ff273e6f5989e8b93d46f
Rhenan Bartels <rhenan.bartels@gmail.com>
2019-12-04 08:59:56 -0300

    Detrend others rri values as normal iterables

--- hrv/detrend.py
+++ hrv/detrend.py
@@ -1,10 +1,16 @@
 import numpy as np
 
-from hrv.rri import RRiDetrended
+from hrv.rri import RRiDetrended, RRi, _create_time_array
 
 
 def polynomial_detrend(rri, degree):
-    coef = np.polyfit(rri.time, rri.values, deg=degree)
-    polynomial = np.polyval(coef, rri.time)
-    detrended_rri = rri.values - polynomial
-    return RRiDetrended(detrended_rri, time=rri.time)
+    if isinstance(rri, RRi):
+        time = rri.time
+        rri = rri.values
+    else:
+        time = _create_time_array(rri)
+
+    coef = np.polyfit(time, rri, deg=degree)
+    polynomial = np.polyval(coef, time)
+    detrended_rri = rri - polynomial
+    return RRiDetrended(detrended_rri, time=time)
--- tests/test_detrend.py
+++ tests/test_detrend.py
@@ -23,3 +23,23 @@ class RRiDetrend(TestCase):
         assert isinstance(detrended_rri, RRiDetrended)
         np.testing.assert_almost_equal(detrended_rri.time, fake_rri.time)
         np.testing.assert_almost_equal(detrended_rri.mean(), 0)
+
+    def test_detrend_with_non_RRi_object(self):
+        fake_rri = [810, 830, 860, 790, 804]
+
+        detrended_rri = polynomial_detrend(fake_rri, degree=3)
+
+        expected_rri = [
+            4.12877839,
+            -16.31947386,
+            25.77155567,
+            -18.14773522,
+            4.56687502
+        ]
+        expected_time = np.array([0.   , 0.83 , 1.69 , 2.48 , 3.284])
+
+        np.testing.assert_almost_equal(detrended_rri, expected_rri)
+        assert isinstance(detrended_rri, RRiDetrended)
+        np.testing.assert_almost_equal(detrended_rri.time, expected_time)
+        np.testing.assert_almost_equal(detrended_rri.mean(), 0)
+
