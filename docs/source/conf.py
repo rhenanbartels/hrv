a0f33e82e6bda256e39829b55e03da5a06408059
Rhenan Bartels <rhenan.bartels@gmail.com>
2020-06-26 22:03:26 -0300

    Bump version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, Rhenan Bartels'
 author = 'Rhenan Bartels'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.6'
+release = '0.2.7'
 
 
 # -- General configuration ---------------------------------------------------
