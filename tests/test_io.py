71883de141f4c37486f45ac21314091a06a75be9
Rhenan Bartels <rhenan.bartels@gmail.com>
2021-07-14 10:19:56 -0300

    Change some tests from pytest style

--- tests/test_io.py
+++ tests/test_io.py
@@ -1,5 +1,6 @@
 import unittest
 
+import pytest
 import numpy as np
 
 from hrv.exceptions import EmptyFileError
@@ -8,19 +9,20 @@ from hrv.rri import RRi
 from tests.test_utils import FAKE_RRI
 
 
-class RRIFileOpeningTestCase(unittest.TestCase):
+class TestRRIFileOpening:
     def test_open_rri_text_file(self):
         rri_file_name = "tests/test_files/test_file_1.txt"
 
         response = read_from_text(rri_file_name)
         expected = np.array(FAKE_RRI)
 
-        self.assertTrue(isinstance(response, RRi))
+        assert isinstance(response, RRi)
         np.testing.assert_equal(response.values, expected)
 
     def test_open_empty_text_file(self):
         rri_file_name = "tests/test_files/empty.txt"
-        self.assertRaises(EmptyFileError, read_from_text, rri_file_name)
+        with pytest.raises(EmptyFileError):
+            read_from_text(rri_file_name)
 
     def test_open_hrm_file(self):
         rri_file_name = "tests/test_files/test_file_2.hrm"
@@ -28,25 +30,29 @@ class RRIFileOpeningTestCase(unittest.TestCase):
         response = read_from_hrm(rri_file_name)
         expected = np.array(FAKE_RRI)
 
-        self.assertTrue(isinstance(response, RRi))
+        assert isinstance(response, RRi)
         np.testing.assert_equal(response.values, expected)
 
     def test_open_empty_hrm_file(self):
         rri_file_name = "tests/test_files/test_file_mistake_2.hrm"
-        self.assertRaises(EmptyFileError, read_from_hrm, rri_file_name)
+        with pytest.raises(EmptyFileError):
+            read_from_hrm(rri_file_name)
 
 
-class OpenRRiFromCsv(unittest.TestCase):
+        np.testing.assert_equal(rri.values, expected)
+
+
+class TestOpenRRiFromCsv:
     def test_open_rri_single_column(self):
         rri = read_from_csv("tests/test_files/rri_1.csv")
 
-        self.assertTrue(isinstance(rri, RRi))
+        assert isinstance(rri, RRi)
         np.testing.assert_equal(rri.values, np.array([790, 815, 800, 795]))
 
     def test_open_rri_single_column_with_header(self):
         rri = read_from_csv("tests/test_files/rri_header.csv", row_offset=1)
 
-        self.assertTrue(isinstance(rri, RRi))
+        assert isinstance(rri, RRi)
         np.testing.assert_equal(rri.values, np.array([790, 815, 800, 795]))
 
     def test_open_rri_multiple_columns(self):
@@ -54,7 +60,7 @@ class OpenRRiFromCsv(unittest.TestCase):
             "tests/test_files/rri_multiple_columns.csv", rri_col_index=1, row_offset=1
         )
 
-        self.assertTrue(isinstance(rri, RRi))
+        assert isinstance(rri, RRi)
         np.testing.assert_equal(rri.values, np.array([790, 815, 800, 795]))
 
     def test_open_rri_with_time(self):
@@ -65,14 +71,14 @@ class OpenRRiFromCsv(unittest.TestCase):
             row_offset=1,
         )
 
-        self.assertTrue(isinstance(rri, RRi))
+        assert isinstance(rri, RRi)
         np.testing.assert_equal(rri.values, np.array([790, 815, 800, 795]))
         np.testing.assert_equal(rri.time, np.array([1.0, 2.0, 3.0, 4.0]))
 
     def test_open_rri_separated_with_semicolon(self):
         rri = read_from_csv("tests/test_files/rri_semicolon.csv", row_offset=1,)
 
-        self.assertTrue(isinstance(rri, RRi))
+        assert isinstance(rri, RRi)
         np.testing.assert_equal(rri.values, [790, 815, 800, 795])
 
     def test_parse_time_column_in_csv(self):
@@ -84,7 +90,7 @@ class OpenRRiFromCsv(unittest.TestCase):
             time_parser=float,
         )
 
-        self.assertTrue(isinstance(rri, RRi))
+        assert isinstance(rri, RRi)
         np.testing.assert_equal(rri.values, np.array([790, 815, 800, 795]))
         np.testing.assert_equal(rri.time, np.array([1.0, 2.0, 3.0, 4.0]))
 
@@ -100,6 +106,6 @@ class OpenRRiFromCsv(unittest.TestCase):
             time_parser=_time_parser,
         )
 
-        self.assertTrue(isinstance(rri, RRi))
+        assert isinstance(rri, RRi)
         np.testing.assert_equal(rri.values, np.array([790, 815, 800, 795]))
         np.testing.assert_equal(rri.time, np.array([56, 57, 58, 59]))
